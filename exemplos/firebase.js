6ae1bc98330e8ba6109fb7450f5e394eba5c72a9
DEVHenriquecs <59708919+DEVHenriquecs@users.noreply.github.com>
2021-07-07 20:02:15 -0300

    Add files via upload

---
+++ exemplos/firebase.js
@@ -0,0 +1,16 @@
+(function(){
+ // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyC2WHo-f_nBw3waDbJfpW0kb3zT6gAtW5Q",
+    authDomain: "acolasa-f7f3d.firebaseapp.com",
+    projectId: "acolasa-f7f3d",
+    storageBucket: "acolasa-f7f3d.appspot.com",
+    messagingSenderId: "1024137247687",
+    appId: "1:1024137247687:web:a0ac1c11dd83377f3e77e2",
+    measurementId: "G-DTYYCBMGFQ"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+})
